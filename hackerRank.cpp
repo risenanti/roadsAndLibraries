bbf8b6914b6279da9c1ea54c6ca55906c51e0a7b
risenanti <risenanti@gmail.com>
2017-10-18 12:17:56 -0500

    submitted correctly

--- hackerRank.cpp
+++ hackerRank.cpp
@@ -1,62 +1,56 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-
-void DFS(int vertice, bool visited[] ,list<int> *adj);
-
+void dfs(int s,bool visited[],vector<int>adj[])
+{
+    visited[s]=true;
+    for(int i=0;i<adj[s].size();i++)
+    {
+        if(visited[adj[s][i]]==false)
+            dfs(adj[s][i],visited,adj);
+    }
+}
+void intialize(bool visited[],int n)
+{
+    for(int i=0;i<n+1;i++)
+        visited[i]=false;
+}
 int main() {
     int q;
     cin >> q;
     for(int a0 = 0; a0 < q; a0++){
-        int numberOfCities;
-        int numberOfRoads;
-        long cLib;
-        long cRoad;
-        cin >> numberOfCities >> numberOfRoads >> cLib >> cRoad;
+        int n;
+        int m;
+        long x;
+        long y;
+        cin >> n >> m >> x >> y;
 
-        if(cRoad>=cLib)
-        {
-            cout<< (cLib*numberOfCities) << endl;
-            return 0;
-        }
+        vector<int>adj[n+1];
+        bool visited[n+1];
+        long connectedcomponents=0;
 
-        list<int> *city = new list<int>[numberOfCities+1];
-        bool *visited = new bool[numberOfCities+1];
-        for (int i = 0; i < numberOfCities; i++)
-        {
-            visited[i] = false;
-        }
-
-        //add edges to graph
-        for(int a1 = 0; a1 < numberOfRoads; a1++){
+        for(int a1 = 0; a1 < m; a1++){
             int city_1;
             int city_2;
             cin >> city_1 >> city_2;
-            city[city_1].push_back(city_2);
-            city[city_2].push_back(city_1);
+            adj[city_2].push_back(city_1);
+            adj[city_1].push_back(city_2);
         }
-
-        DFS(1, visited, city);
-
-        for (int i = 1; i < numberOfCities+1; i++)
+     if(y>=x)
+            cout<<n*x<<endl;
+        else
         {
-        	cout<< visited[i] <<" "<<endl;
+        intialize(visited,n);
+        for(int i=1;i<=n;i++)
+        {
+            if(visited[i]==false)
+            {
+             dfs(i,visited,adj);
+             ++connectedcomponents;
+             }
         }
-
-        delete[] city;
-        delete[] visited;
+        cout<<y*(n-connectedcomponents)+x*connectedcomponents<<endl;
+      }
     }
     return 0;
-}
-
-void DFS(int vertice, bool visited[] ,list<int> *adj)
-{
-	visited[vertice] = true;
-
-	//process node
-
-	list<int>::iterator i;
-	for(i = adj[vertice].begin(); i != adj[vertice].end(); ++i)
-	{
-		DFS(*i, visited, adj);
-	}
 }
